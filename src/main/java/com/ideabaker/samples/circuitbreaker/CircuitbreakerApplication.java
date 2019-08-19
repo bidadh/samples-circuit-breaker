b1a8cb429ea43ae6c80b3a446b96fc3c90b92d14
Arthur Kazemi <bidadh@gmail.com>
2019-08-19 12:09:23 +1000

    add basic FailingService and FailingController

--- src/main/java/com/ideabaker/samples/circuitbreaker/CircuitbreakerApplication.java
+++ src/main/java/com/ideabaker/samples/circuitbreaker/CircuitbreakerApplication.java
@@ -1,7 +1,17 @@
 package com.ideabaker.samples.circuitbreaker;
 
+import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.commons.ReactiveCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.commons.ReactiveCircuitBreakerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class CircuitbreakerApplication {
@@ -10,4 +20,31 @@ public class CircuitbreakerApplication {
 		SpringApplication.run(CircuitbreakerApplication.class, args);
 	}
 
+}
+
+@RestController
+class FailingController {
+	private final FailingService failingService;
+	private final ReactiveCircuitBreaker circuitBreaker;
+
+	FailingController(FailingService failingService, ReactiveCircuitBreakerFactory cbf) {
+		this.failingService = failingService;
+		this.circuitBreaker = cbf.create("greet");
+	}
+
+	@GetMapping("/greet")
+	Publisher<String> greet(@RequestParam(required = false) String name) {
+		var results = failingService.greet(name); //cold
+
+		return this.circuitBreaker.run(results, throwable -> Mono.just("Hello world!"));
+	}
+}
+
+@Service
+class FailingService {
+	Mono<String> greet(String name) {
+		return Optional.ofNullable(name)
+				.map(n -> Mono.just("Hello " + n + "!"))
+				.orElse(Mono.error(new IllegalArgumentException()));
+	}
 }
